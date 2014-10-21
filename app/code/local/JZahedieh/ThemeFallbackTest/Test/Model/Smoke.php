30eab60ab439da7c2a8f95f759387e446071631d
Jonathan Zahedieh <jonathan.zahedieh@tryzens.com>
2014-10-21 16:32:02 +0100

    Base of module w smoke test

---
+++ app/code/local/JZahedieh/ThemeFallbackTest/Test/Model/Smoke.php
@@ -0,0 +1,9 @@
+<?php
+
+class JZahedieh_ThemeFallbackTest_Test_Model_Smoke extends EcomDev_PHPUnit_Test_Case
+{
+    public function testSmoke()
+    {
+        $this->assertEquals(1, 1);
+    }
+}
