46485cd9e7ea0abdd0fe42b5a701f1a2f8f1b309
Jonathan Zahedieh <jonathan.zahedieh@tryzens.com>
2014-10-21 16:47:08 +0100

    Smoke test should be controller test case

---
+++ app/code/local/JZahedieh/ThemeFallbackTest/Test/Controller/Block.php
@@ -0,0 +1,9 @@
+<?php
+
+class JZahedieh_ThemeFallbackTest_Test_Controller_Block extends EcomDev_PHPUnit_Test_Case_Controller
+{
+    public function testSmoke()
+    {
+        $this->assertEquals(1, 1);
+    }
+}
--- app/code/local/JZahedieh/ThemeFallbackTest/Test/Model/Smoke.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class JZahedieh_ThemeFallbackTest_Test_Model_Smoke extends EcomDev_PHPUnit_Test_Case
-{
-    public function testSmoke()
-    {
-        $this->assertEquals(1, 1);
-    }
-}
