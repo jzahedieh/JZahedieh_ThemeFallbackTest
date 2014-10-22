91f97e2c787d9a920f0efcda044a1bdc75af2bea
Jonathan Zahedieh <jonathan.zahedieh@tryzens.com>
2014-10-22 11:05:31 +0100

    Issue was with symlinks, tests to prove

---
+++ app/code/local/JZahedieh/ThemeFallbackTest/Test/Controller/Symlink.php
@@ -0,0 +1,82 @@
+<?php
+
+class JZahedieh_ThemeFallbackTest_Test_Controller_Symlink extends EcomDev_PHPUnit_Test_Case_Controller
+{
+    /**
+     * Using default package and no symlinks, should pass
+     *
+     * @loadFixture default_package.yaml
+     * @loadFixture disable_symlinks.yaml
+     */
+    public function testDispatch()
+    {
+        $this->dispatch('');
+        $this->assertRequestRoute('cms/index/index');
+
+        $this->assertEquals('rwd', Mage::getStoreConfig('design/package/name'));
+
+        $this->_assertLayoutAndResponse();
+    }
+
+    /**
+     * Test with a custom package with rewritten 1column.phtml brought in using a symlink.
+     *
+     * Will fail due to trying to include the file from the composer vendor file:
+     *
+     * @see         Mage_Core_Block_Template::fetchView() line 239-244
+     *
+     *   $includeFilePath = realpath($this->_viewDir . DS . $fileName);
+     *   if (strpos($includeFilePath, realpath($this->_viewDir)) === 0 || $this->_getAllowSymlinks()) {
+     *       include $includeFilePath;
+     *   } else {
+     *       Mage::log('Not valid template file:'.$fileName, Zend_Log::CRIT, null, null, true);
+     *   }
+     *
+     * @loadFixture unittest_package.yaml
+     * @loadFixture disable_symlinks.yaml
+     */
+    public function testDispatchUnittestPackageWithoutSymlinkConfig()
+    {
+        $this->dispatch('');
+        $this->assertRequestRoute('cms/index/index');
+
+        $this->assertEquals('unittest', Mage::getStoreConfig('design/package/name'));
+        $this->_assertLayoutAndResponse();
+    }
+
+    /**
+     * With symlinks enabled should pass.
+     *
+     * @loadFixture unittest_package.yaml
+     * @loadFixture enable_symlinks.yaml
+     */
+    public function testDispatchUnittestPackageWithSymlinkConfig()
+    {
+        $this->dispatch('');
+        $this->assertRequestRoute('cms/index/index');
+
+        $this->assertEquals('unittest', Mage::getStoreConfig('design/package/name'));
+        $this->_assertLayoutAndResponse();
+
+    }
+
+    /**
+     * Assertions by Ivan that check everything the homepage is working correctly
+     *
+     * @see https://github.com/EcomDev/EcomDev_PHPUnit/issues/60
+     */
+    protected function _assertLayoutAndResponse()
+    {
+        $this->assertLayoutBlockCreated('left');
+        $this->assertLayoutBlockCreated('right');
+        $this->assertLayoutBlockRendered('content');
+        $this->assertLayoutBlockTypeOf('left', 'core/text_list');
+        $this->assertLayoutBlockNotTypeOf('left', 'core/links');
+
+        $this->assertResponseBodyContains('Magento');
+        $this->assertResponseBodyContains('Home Page');
+        $this->assertResponseBodyContains('Compare Products');
+        $this->assertResponseBodyNotContains('Non existing text');
+    }
+
+}
